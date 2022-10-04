047a748f9a93562f3e0982932d7944c688af08b0
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2022-10-04 17:20:29 -0300

    Add flux test

---
+++ src/test/kotlin/com/codengine/reactive/poc/FiterTest.kt
@@ -0,0 +1,25 @@
+package com.codengine.reactive.poc
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+
+class FiterTest {
+    private val cityList = listOf("Ipatinga", "BH", "Uberlandia", "Sao Paulo", "Passa dez")
+
+    @Test
+    fun filterSmallNameCitiesTest(){
+        val fluxOfCities = Flux.fromIterable(cityList)
+        val filteredCities = fluxOfCities.filter { city -> city.length <= 2}
+
+        StepVerifier.create(filteredCities).expectNext("BH").verifyComplete()
+    }
+
+    @Test
+    fun filterCitiesNameThatStartWithITest(){
+        val fluxOfCities = Flux.fromIterable(cityList)
+        val filteredCities = fluxOfCities.filter { city -> city.startsWith("I")}
+
+        StepVerifier.create(filteredCities).expectNext("Ipatinga").verifyComplete()
+    }
+}

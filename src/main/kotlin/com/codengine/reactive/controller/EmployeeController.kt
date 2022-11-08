a2bb5c8927834d46ba08a5aa718639b18aba2e41
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2022-11-08 15:50:58 -0300

    Criacao de CRUD de empregados

--- src/main/kotlin/com/codengine/reactive/controller/EmployeeController.kt
+++ src/main/kotlin/com/codengine/reactive/controller/EmployeeController.kt
@@ -3,10 +3,9 @@ package com.codengine.reactive.controller
 import com.codengine.reactive.model.Employee
 import com.codengine.reactive.repository.EmployeesRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("v1/employees")
@@ -18,7 +17,27 @@ class EmployeeController(
     @GetMapping
     fun getAllEmployees(): Flux<Employee> {
         return employeeRepository.findAll()
-        //TODO criar imagem docker do mongo db
     }
+
+    @GetMapping("{id}")
+    fun getEmployeeById(@PathVariable id: String): Mono<Employee> {
+        return employeeRepository.findById(id)
+    }
+
+    @PostMapping
+    fun insertEmployee(@RequestBody employee: Employee): Mono<Employee> {
+        return employeeRepository.save(employee)
+    }
+
+    @PutMapping("/update")
+    fun updateEmployee(@RequestBody employee: Employee): Mono<Employee> {
+        return employeeRepository.save(employee)
+    }
+
+    @DeleteMapping("/delete/{id}")
+    fun deleteEmployee(@PathVariable id: String): Mono<Void> {
+        return employeeRepository.deleteById(id)
+    }
+
 
 }

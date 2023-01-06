3067a6abb2664ca4ff47caa44122b545ca67b315
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2023-01-06 18:14:23 -0300

    Refactor

--- src/main/kotlin/com/codengine/reactive/config/ProducerKafkaConfig.kt
+++ src/main/kotlin/com/codengine/reactive/config/ProducerKafkaConfig.kt
@@ -1,11 +1,14 @@
 package com.codengine.reactive.config
 
+import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaAdmin
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.support.serializer.JsonSerializer
@@ -13,18 +16,15 @@ import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
 class ProducerKafkaConfig(
-    /*@Autowired
-    private val kafkaProperties : KafkaProperties*/
+    @Autowired
+    private val kafkaProperties : KafkaProperties
 
 ) {
-    /*@Value("\${kafka.bootstrap-servers}")
-    private lateinit var servers: String*/
-    private val servers = "localhost:29092"
 
     @Bean
     fun producerFactory(): ProducerFactory<String, Any> {
         val configProps: MutableMap<String, Any> = HashMap()
-        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = servers
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
         configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
         return DefaultKafkaProducerFactory(configProps)
@@ -33,6 +33,13 @@ class ProducerKafkaConfig(
     @Bean
     fun kafkaTemplate(): KafkaTemplate<String, Any> {
         return KafkaTemplate(producerFactory())
+    }
+
+    @Bean // Possibilita criar topicos a partir do produtor
+    fun kafkaAdmin(): KafkaAdmin? {
+        val configs = HashMap<String, Any>()
+        configs[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
+        return KafkaAdmin(configs)
     }
 
 }

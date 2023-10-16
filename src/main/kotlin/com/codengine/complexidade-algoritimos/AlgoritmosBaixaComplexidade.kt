f176264ab9dad1b0bf69c5e0bb2ce230b0cd0b31
adriangonc <adriangonc@yahoo.com.br>
2023-10-16 18:19:56 -0300

    Ajustes

--- src/main/kotlin/com/codengine/complexidade-algoritimos/AlgoritmosBaixaComplexidade.kt
+++ src/main/kotlin/com/codengine/complexidade-algoritimos/AlgoritmosBaixaComplexidade.kt
@@ -22,3 +22,22 @@ fun findElementInArrayOfArrays(numberArray: IntArray, elementArray: IntArray): A
     //Complexidade O(N²)
 }
 
+fun findElementIn3Arrays(numberArray: IntArray, elementArray: IntArray, elementArray3: IntArray): Array<Number> {
+    var elementsInTwoArrays: Array<Number> = arrayOf()
+    for (n in numberArray.indices) { // O(N)
+        for (j in elementArray.indices) { // O(N)
+            for(i in elementArray3.indices){
+                if (numberArray[n] == elementArray[j]) {
+                    elementsInTwoArrays = elementsInTwoArrays.plus(elementArray[j])
+                    continue
+                } else if (elementArray[n] == elementArray3[i]){
+                    elementsInTwoArrays = elementsInTwoArrays.plus(elementArray3[i])
+                }
+            }
+        }
+    }
+
+    return elementsInTwoArrays
+    //Complexidade O(N²)
+}
+
--- src/test/kotlin/com/codengine/reactive/algoritmos/ComplexidadeAlgoritmosTest.kt
+++ src/test/kotlin/com/codengine/reactive/algoritmos/ComplexidadeAlgoritmosTest.kt
@@ -1,5 +1,6 @@
 package com.codengine.reactive.algoritmos
 
+import com.codengine.`complexidade-algoritimos`.findElementIn3Arrays
 import com.codengine.`complexidade-algoritimos`.findElementInArrayOfArrays
 import com.codengine.`complexidade-algoritimos`.findElementInVectorOfInts
 import org.junit.Before
@@ -14,6 +15,7 @@ class ComplexidadeAlgoritmosTest {
 
     private var numberArray = intArrayOf(0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 1532, 9, 45, 78)
     private var elementsArray = intArrayOf(-8, 1, 2, 3, 4, 1532, 36, 89, 45, 123, 97, 456, 23, 12, 389, 324, 1596)
+    private var elementsArray3 = intArrayOf(-18, 19, -2, 38, 44, 1532, 36, 89, 45, 123, 97, 456, 23, 12, 389, 324, 1596)
 
     @Before
     fun setUp() {
@@ -33,4 +35,10 @@ class ComplexidadeAlgoritmosTest {
         testResult?.forEach { n -> println(n) }
     }
 
+    @Test
+    fun algoritmoN3Test() {
+
+        var testResult = findElementIn3Arrays(numberArray, elementsArray, elementsArray3)
+        testResult?.forEach { n -> println(n) }
+    }
 }
